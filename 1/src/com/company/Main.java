695e3a14c4c49243be51fb36ecb649b65bfa1de9
unknown <kimokamha@yahoo.com>
2021-05-22 12:27:17 +0200

    first commit

---
+++ 1/src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner o = new Scanner(System.in);
+        System.out.println("Enter Email");
+
+        String Email = o.nextLine();
+        System.out.println("Email is: " + Email);
+
+
+
+
+
+        Scanner ka = new Scanner(System.in);
+        System.out.println("Enter Password");
+
+        String Password = ka.nextLine();
+        System.out.println("Password is: " + Password);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
---
+++ multiplie/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int num1 = 9;
+            int num2 = 4;
+            int num3 = 7;
+            int result = num1 - num3 - num2 +14;
+        System.out.println(result);
+
+    }
+}
